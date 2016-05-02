7c66ac9bb9ac5d7e4f360d351748a6ff2c18d4ef
mcmspark <mcmspark@hotmail.com>
2016-05-02 15:21:34 -0400

    Initial Commit
    A good starting point

---
+++ FEZHATtest/MainPage.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using GHIElectronics.UWP.Shields;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FEZHATtest
+{
+    enum RobotAction
+    {
+        forward,
+        turnleft,
+        turnright,
+        backward
+    }
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private FEZHAT hat;
+        private DispatcherTimer timer;
+        private bool next;
+        private RobotAction currentAction;
+        private int speedmode = 0;
+        private int debounce = 0 ;
+        private UltrasonicDistanceSensor dist;
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.Setup();
+        }
+
+        private async void Setup()
+        {
+            this.hat = await FEZHAT.CreateAsync();
+            this.dist = new UltrasonicDistanceSensor(hat,false);
+
+            this.hat.S1.SetLimits(500, 2400, 0, 180);
+            this.hat.S2.SetLimits(500, 2400, 0, 180);
+
+            this.currentAction = RobotAction.forward;
+
+            this.timer = new DispatcherTimer();
+            this.timer.Interval = TimeSpan.FromMilliseconds(100);
+            this.timer.Tick += this.OnTick;
+            this.timer.Start();
+        }
+
+        private async void OnTick(object sender, object e)
+        {
+            var distval = await dist.GetDistanceInCmAsync(50);
+            UpdateDisplay(distval);
+
+            this.next = !this.next;
+
+            CheckButtons();
+
+            PerformAction(distval);
+        }
+
+        private void PerformAction(double distval)
+        {
+            if (distval > 20 && currentAction==RobotAction.forward)
+            {
+                hat.MotorB.Speed = hat.MotorA.Speed = speedmode * .10 + .30;
+            }
+            if(distval<20)
+            {
+                switch (currentAction)
+                {
+                    case RobotAction.forward:
+                        hat.MotorA.Speed = speedmode * .10 + .30;
+                        hat.MotorB.Speed = -hat.MotorA.Speed;
+                        currentAction = RobotAction.turnright;
+                        break;
+                    case RobotAction.backward:
+                        break;
+                    case RobotAction.turnleft:
+                        break;
+                    case RobotAction.turnright:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (distval > 20)
+            {
+                currentAction = RobotAction.forward;
+            }
+        }
+
+        private void CheckButtons()
+        {
+            if (this.hat.IsDIO18Pressed() && this.hat.IsDIO22Pressed())
+            {
+                this.hat.D2.Color = FEZHAT.Color.Black;
+                this.hat.D3.Color = FEZHAT.Color.Black;
+                this.hat.MotorA.Speed = 0.0;
+                this.hat.MotorB.Speed = 0.0;
+                Application.Current.Exit();
+            }
+            if (this.hat.IsDIO18Pressed() && debounce == 0)
+            {
+                debounce = 3;
+                speedmode++;
+                if (speedmode > 2) speedmode = 0;
+
+            }
+            else
+            {
+                debounce--;
+                if (debounce < 0) debounce = 0;
+            }
+
+            if (this.hat.IsDIO22Pressed())
+            {
+            }
+            else
+            {
+            }
+        }
+
+        private void UpdateDisplay(double distval)
+        {
+            double x, y, z;
+            this.hat.GetAcceleration(out x, out y, out z);
+            this.LightTextBox.Text = this.hat.GetLightLevel().ToString("P2");
+            this.TempTextBox.Text = this.hat.GetTemperature().ToString("N2");
+            this.AccelTextBox.Text = $"({x:N2}, {y:N2}, {z:N2})";
+            this.Button18TextBox.Text = this.hat.IsDIO18Pressed().ToString();
+            this.Button22TextBox.Text = this.hat.IsDIO22Pressed().ToString();
+            this.AnalogTextBox.Text = this.hat.ReadAnalog(FEZHAT.AnalogPin.Ain1).ToString("N2");
+            this.LedsTextBox.Text = ""; //String.Format("({0},{1},{2}) and ({3},{4},{5})", colors[0], colors[1], colors[2], colors[3], colors[4], colors[5]);
+            this.DistanceTextBox.Text = distval.ToString();
+            this.LedsTextBox.Text = this.next.ToString();
+            this.hat.DIO24On = this.next;
+
+            this.hat.D2.Color = (distval < 20 ? FEZHAT.Color.Red : distval < 40 ? FEZHAT.Color.Yellow : FEZHAT.Color.Blue);
+            this.hat.D3.Color = (speedmode == 0 ? FEZHAT.Color.Red : speedmode == 1 ? FEZHAT.Color.Yellow : FEZHAT.Color.Green);
+
+        }
+    }
+}
---
+++ FEZHATtest/UltrasonicDistanceSensor.cs
@@ -0,0 +1,108 @@
+﻿using GHIElectronics.UWP.Shields;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+
+namespace FEZHATtest
+{
+    //
+    // MATH:
+    //   Distance is (Speed of Sound) * Time
+    //   Sound is 340.3 m/s
+    //   Round Trip Distance = 340.3 m/s * Seconds
+    //   Distance to Object in cm = (34030 cm/s * Seconds)/2
+    //   Distance to Object in cm = (34030 cm/s * ticks/(ticks per second))/2
+    //   resolution  in ticks is = (resolution in cm)/34030*(ticks per second)*2
+    //
+    // WIRING:
+    //   Trigger (white wire) on GPIO 26
+    //   Echo (black wire) on GPIO 16
+    //
+    public class UltrasonicDistanceSensor
+    {
+        private readonly Stopwatch _stopwatch;
+        private readonly FEZHAT _hat;
+        private readonly long _tick_resolution;
+        private readonly long _ticks_per_second;
+        private const double PULSE_WIDTH_ms = 3.0; // sound from 1m away should be back in 3ms
+        private const double SPEED_OF_SOUND_cmps = 34030.0; // cm/sec
+        private const double RESOLUTION_cm = 0.5; // cm
+        private const double MAX_DISTANCE_cm = 10000.0;
+        private struct Edge
+        {
+            public const bool leading = true;
+            public const bool trailing = false;
+        }
+        private GpioPin _echoPin;
+        private long _ticks;
+        private bool _useEvent;
+        private bool _complete;
+
+        public UltrasonicDistanceSensor(FEZHAT hat, bool useEvent=false)
+        {
+            _stopwatch = new Stopwatch();
+            _ticks_per_second = Stopwatch.Frequency;
+            _tick_resolution = (long)( RESOLUTION_cm * _ticks_per_second / SPEED_OF_SOUND_cmps * 2.0 );
+            _hat = hat;
+            _hat.WriteDigital(FEZHAT.DigitalPin.DIO26, false); // clear trigger
+            _useEvent = useEvent;
+            if (_useEvent)
+            {
+                //_echoPin = _hat.Dio16;
+                //_echoPin.SetDriveMode(GpioPinDriveMode.Input);
+                //_echoPin.ValueChanged += _echoPin_ValueChanged;
+            }
+        }
+
+        private void _echoPin_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
+        {
+            if (args.Edge == GpioPinEdge.FallingEdge)
+            {
+                _ticks = _stopwatch.ElapsedTicks;
+                _complete = true;
+            }
+        }
+        public async Task<double> GetDistanceInCmAsync(int timeoutInMilliseconds)
+        {
+            double distance = MAX_DISTANCE_cm;
+            _ticks=0L;
+            _complete = false;
+            long ticktimeout = timeoutInMilliseconds * _ticks_per_second / 1000;
+            bool find_edge = Edge.leading;
+            try
+            {
+                _stopwatch.Reset();
+                await SendPulse(PULSE_WIDTH_ms);  
+                _stopwatch.Start();
+                while (_ticks < ticktimeout)
+                {
+                    if (_useEvent && _complete)
+                    {
+                        break;
+                    }
+                    else if(find_edge == _hat.ReadDigital(FEZHAT.DigitalPin.DIO16))
+                    {
+                        if (find_edge) find_edge = Edge.trailing;
+                        else break;
+                    }
+                    await Task.Delay(TimeSpan.FromTicks(_tick_resolution)); 
+                    _ticks = _stopwatch.ElapsedTicks;
+                }
+            }
+            finally
+            {
+                _stopwatch.Stop();
+                distance = _ticks * SPEED_OF_SOUND_cmps / _ticks_per_second / 2.0;
+            }
+            return Math.Round(distance*10.0)/10.0; // round to 1 decimal place
+        }
+
+        private async Task SendPulse(double milliseconds)
+        {
+            _hat.WriteDigital(FEZHAT.DigitalPin.DIO26, true); // trigger
+            await Task.Delay(TimeSpan.FromMilliseconds(milliseconds));
+            _hat.WriteDigital(FEZHAT.DigitalPin.DIO26, false); // trigger
+        }
+    }
+}
